2d7c11576253d23a30567f1a1803120410ec2ddd
Orlilol <florian.respaud@epitech.eu>
2015-11-07 13:30:29 +0100

    commit comment

---
+++ OCR/OCR.Shared/pch.h
@@ -0,0 +1,11 @@
+﻿//
+// pch.h
+// En-tête pour fichiers Include système standard.
+//
+
+#pragma once
+
+#include <collection.h>
+#include <ppltasks.h>
+
+#include "App.xaml.h"
---
+++ OCR/OCR.WindowsPhone/MainPage.xaml.cpp
@@ -0,0 +1,45 @@
+﻿//
+// MainPage.xaml.cpp
+// Implémentation de la classe MainPage.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace OCR;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// Pour en savoir plus sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=234238
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+/// <summary>
+/// Invoqué lorsque cette page est sur le point d'être affichée dans un frame.
+/// </summary>
+/// <param name="e">Données d'événement décrivant la manière dont l'utilisateur a accédé à cette page. La propriété Parameter
+/// est généralement utilisée pour configurer la page.</param>
+void MainPage::OnNavigatedTo(NavigationEventArgs^ e)
+{
+	(void) e;	// Paramètre non utilisé
+
+	// TODO: préparer la page pour affichage ici.
+
+	// TODO: si votre application comporte plusieurs pages, assurez-vous que vous
+	// gérez le bouton Retour physique en vous inscrivant à l’événement
+	// Événement Windows::Phone::UI::Input::HardwareButtons.BackPressed.
+	// Si vous utilisez le NavigationHelper fourni par certains modèles,
+	// cet événement est géré automatiquement.
+}
