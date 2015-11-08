cb551a5cb74dd3ba9e307e58914a707c30f26f31
Ruben Renzema <ruben.renzema@gmail.com>
2015-11-08 17:58:17 +0100

    KNN impl but fails

--- OCR/MainWindow.xaml.cs
+++ OCR/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Shapes;
 using System.Diagnostics;
 using System.IO;
 using System.Drawing.Imaging;
+using Accord.MachineLearning;
 
 namespace OCR
 {
@@ -44,6 +45,7 @@ namespace OCR
         }
 
     }
+
     public partial class MainWindow : Window
     {
         public int grayLimit = 200;
@@ -59,62 +61,77 @@ namespace OCR
         {
             OpenFileDialog dlg = new OpenFileDialog();
 
-            dlg.DefaultExt = ".jpg";
-            dlg.Filter = "All images|*.jpeg;*.png;*.jpg|JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|Bitmap Files (*.bmp)|*.bmp";
+            dlg.Filter = "All images|*.jpeg;*.png;*.jpg;*.bmp|JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|Bitmap Files (*.bmp)|*.bmp";
             Nullable<bool> result = dlg.ShowDialog();
             if (result == true)
             {
                 string filename = dlg.FileName;
-                FileNameTextBox.Text = filename;
-                this.bitmap = getImageBitmap(filename);
                 Uri uri = new Uri(filename);
                 BitmapImage imgSource = new BitmapImage(uri);
-                picture.Source = imgSource;
+
+                FileNameTextBox.Text = filename;
+                this.bitmap = getImageBitmap(filename);
                 this.bitmap = this.cropImage();
+                picture.Source = imgSource;
                 pictureRes.Source = Tools.ToBitmapImage(this.bitmap);
-                this.test();
-                /* code here */
+
+                /* KNN */
+                double[][] inputs = {
+                    getVectors(getImageBitmap(filename))[0],
+                
+                    getVectors(getImageBitmap(filename))[0]
+                };
+
+                int[] outputs = {
+                    0,
+                    1
+                };
+
+                KNearestNeighbors knn = new KNearestNeighbors(k: 4, classes: 2,
+                    inputs: inputs, outputs: outputs);
+
+
+                int answer = knn.Compute(getVectors(getImageBitmap(filename))[0]);
+                Console.WriteLine("a = " + answer);
             }
         }
-        private void test()
-        {
-            int x, y;
-            int xp, yp;
-            xp = yp = 0;
-            List<int>[] vectors = new List<int>[2];
-            vectors[0] = new List<int>();
-            vectors[1] = new List<int>();
 
-            for (x = 0; x < this.bitmap.Width; x++)
-            {
+        private double[][] getVectors(Bitmap source) {
+            int x;
+            int y;
+            int xp = 0;
+            int yp = 0;
+            double[][] res = new double[2][];
+            List<double>[] vectors = new List<double>[2];
+
+            vectors[0] = new List<double>();
+            vectors[1] = new List<double>();
+            for (x = 0; x < this.bitmap.Width; x++) {
                 vectors[0].Add(0);
-                for (y = 0; y < this.bitmap.Height; y++)
-                {
+                for (y = 0; y < this.bitmap.Height; y++) {
                     System.Drawing.Color pixelColor = this.bitmap.GetPixel(x, y);
-                    if (pixelColor.R <= this.grayLimit)
-                    {
+                    if (pixelColor.R <= this.grayLimit) {
                         vectors[0][xp] += 1;
                     }
                 }
                 xp++;
             }
-
-            for (y = 0; y < this.bitmap.Height; y++)
-            {
+            for (y = 0; y < this.bitmap.Height; y++) {
                 vectors[1].Add(0);
-                for (x = 0; x < this.bitmap.Width; x++)
-                {
+                for (x = 0; x < this.bitmap.Width; x++) {
                     System.Drawing.Color pixelColor = this.bitmap.GetPixel(x, y);
-                    if (pixelColor.R <= this.grayLimit)
-                    {
+                    if (pixelColor.R <= this.grayLimit) {
                         vectors[1][yp] += 1;
                     }
                 }
                 yp++;
             }
-            vectors[0].ForEach(i => Console.Write("{0}\t", i));
-            Console.WriteLine(vectors[0].ToString());
+//            vectors[0].ForEach(i => Console.Write("{0}\t", i));
+            res[0] = vectors[0].ToArray();
+            res[1] = vectors[1].ToArray();
+            return res;
         }
+
         private Bitmap cropImage()
         {
             int x, y;
@@ -137,10 +154,10 @@ namespace OCR
                     }
                 }
             }
-            Console.WriteLine(top.ToString());
+            /*Console.WriteLine(top.ToString());
             Console.WriteLine(bottom.ToString());
             Console.WriteLine(left.ToString());
-            Console.WriteLine(right.ToString());
+            Console.WriteLine(right.ToString());*/
 
             var crop = new System.Drawing.Rectangle(left, top, right - left, bottom - top);
 
