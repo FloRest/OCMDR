b91d80bb6b31347149e385fd26c1a39447bc6a37
unknown <nicolas.soares@epitech.eu>
2015-11-11 17:07:48 +0100

    merge grayscale

--- OCR/MainWindow.xaml.cs
+++ OCR/MainWindow.xaml.cs
@@ -62,6 +62,41 @@ namespace OCR
                 Console.WriteLine("ERROR: Directory not found");
             }
         }
+        public static Bitmap MakeGrayscale(Bitmap original)
+        {
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+
+            //get a graphics object from the new image
+            Graphics g = Graphics.FromImage(newBitmap);
+
+            //create the grayscale ColorMatrix
+            ColorMatrix colorMatrix = new ColorMatrix(
+               new float[][]
+               {
+                 new float[] {.3f, .3f, .3f, 0, 0},
+                 new float[] {.59f, .59f, .59f, 0, 0},
+                 new float[] {.11f, .11f, .11f, 0, 0},
+                 new float[] {0, 0, 0, 1, 0},
+                 new float[] {0, 0, 0, 0, 1}
+               });
+
+            //create some image attributes
+            ImageAttributes attributes = new ImageAttributes();
+
+            //set the color matrix attribute
+            attributes.SetColorMatrix(colorMatrix);
+
+            //draw the original image on the new image
+            //using the grayscale color matrix
+            g.DrawImage(original, new System.Drawing.Rectangle(0, 0, original.Width, original.Height),
+               0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+
+            //dispose the Graphics object
+            g.Dispose();
+            return newBitmap;
+        }
+
 
         private void browse_Click(object sender, RoutedEventArgs e)
         {
@@ -106,8 +141,9 @@ namespace OCR
                 }
 
                 KNearestNeighbors knn = new KNearestNeighbors(k: 3, classes: indexes.Count, inputs: letters.ToArray(), outputs: outputList.ToArray());
-                int answer = knn.Compute(getVectors(cropImage(getImageBitmap(filename))));
-                Console.WriteLine("a = " + answer);
+                int answer = knn.Compute(getVectors(cropImage(MakeGrayscale(getImageBitmap(filename)))));
+                string res = indexes[answer];
+                Console.WriteLine(res);
             }
         }
 
